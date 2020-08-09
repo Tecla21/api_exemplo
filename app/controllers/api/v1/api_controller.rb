bcaf0d8d9bac4f1bec6b132e40c9adeeedd0ac08
muatsoftgit <amuatelembe@gmail.com>
2020-08-09 15:13:15 +0100

    conclusao da autenticacao

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -1,7 +1,9 @@
 module Api::V1
     class ApiController < ApplicationController
         # Métodos globais
-        acts_as_token_authentication_handler_for User
+        acts_as_token_authentication_handler_for User, fallback_to_devise: false
+        respond_to :json
+        protect_from_forgery with: :null_session
  
         before_action :require_authentication!
         
---
+++ app/controllers/users/sessions_controller.rb
@@ -0,0 +1,13 @@
+class Users::SessionsController < Devise::SessionsController
+
+    def create
+      self.resource = warden.authenticate!(auth_options)
+      #set_flash_message(:notice, :signed_in) if is_flashing_format?
+      sign_in(resource_name, resource)
+      yield resource if block_given?
+      
+      render :json=> {:success=>true, :auth_token=>resource.authentication_token, :first_name=>resource.first_name, :last_name=>resource.last_name, :email=>resource.email}
+      return
+    end
+  
+end
